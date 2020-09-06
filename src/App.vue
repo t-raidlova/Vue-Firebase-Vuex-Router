b0e6f82314d710836f8b11e136bb694ce3591ee8
unknown <tereza.raidlova@gmail.com>
2020-09-06 10:41:34 +0200

    header component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <app-header />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    appHeader: Header,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Header.vue
@@ -0,0 +1,61 @@
+<template>
+  <header>
+    <div class="header_logo">
+      <img src="../assets/images/planet.png" alr="planet icon" />
+      <h1 href="/">Logo</h1>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <a href="/">home</a>
+        </li>
+        <span>~</span>
+        <li>
+          <a href="/menu">menu</a>
+        </li>
+        <span>~</span>
+        <li>
+          <a href="/contact">contact</a>
+        </li>
+        <span>~</span>
+        <li>
+          <a href="/about">about us</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "appHeader",
+};
+</script>
+
+<style scoped>
+h1 {
+  font-size: 1.3em;
+}
+.header_logo {
+  background-color: #21d4fd;
+  background-image: linear-gradient(19deg, #21d4fd 0%, #b721ff 100%);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 10px;
+}
+.header_logo img {
+  max-width: 25vw;
+}
+ul {
+  padding: 10px 0;
+}
+li {
+  display: inline-block;
+}
+@media screen and (min-width: 900px) {
+  .header_logo img {
+    max-width: 20vw;
+  }
+}
+</style>
